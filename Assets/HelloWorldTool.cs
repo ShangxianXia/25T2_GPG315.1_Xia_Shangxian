5f92aace76eba39e38a0a20c8c724bf8564bc0ec
Shangxian Xia <1032883@student.sae.edu.au>
2025-06-13 15:42:17 +1000

    - added some dynamic sliders for value - used to be only 1 - 10 for scale now can be set from 1 to max 100 or lower - slider for rotation as well 0 to max 360 can be lower - renamed tool to random stuff since its the main point

--- Assets/HelloWorldTool.cs
+++ Assets/HelloWorldTool.cs
@@ -2,23 +2,31 @@ using UnityEngine;
 using UnityEditor;
 using UnityEngine.UI;
 
-public class HelloWorldWindow : EditorWindow
+public class RandomStuffWindow : EditorWindow
 {
-    [MenuItem("Window/Hello World")]
+    private float scaleValue = 1;
+    
+    private float rotationValue = 360;
+    
+    [MenuItem("Window/RandomStuff")]
     public static void ShowWindow()
     {
-        GetWindow<HelloWorldWindow>("Hello World");
+        GetWindow<RandomStuffWindow>("Random stuff");
     }
-
+    
     void OnGUI()
     {
         // GUILayout.Label("Hello World!", EditorStyles.boldLabel);
-        GUILayout.Label("Reset Stuff", EditorStyles.boldLabel);
+        
         if (GUILayout.Button("Close"))
         {
             Close();
         }
         
+        GUILayout.Space(10f);
+        GUILayout.Label("Reset Stuff", EditorStyles.boldLabel);
+        
+        GUILayout.BeginHorizontal();
         if (GUILayout.Button("Reset all"))
         {
             Selection.activeTransform.transform.position = Vector3.zero;
@@ -37,60 +45,81 @@ public class HelloWorldWindow : EditorWindow
         {
             Selection.activeTransform.transform.localScale = Vector3.one;
         }
+
+        if (GUILayout.Button("Reset colour"))
+        {
+            Selection.activeGameObject.GetComponent<MeshRenderer>().material.color = Color.white;
+        }
+        GUILayout.EndHorizontal();
+        
         
         GUILayout.Space(10);
-        GUILayout.Label("Rotation Stuff", EditorStyles.boldLabel);
-        if (GUILayout.RepeatButton("Randomise Rotation X Axis"))
+        GUILayout.Label($"Randomise Rotation Stuff by > {rotationValue} < degrees", EditorStyles.boldLabel);
+        rotationValue = GUILayout.HorizontalSlider(rotationValue, 0, 360f);
+        GUILayout.Space(15);
+        GUILayout.BeginHorizontal();
+        if (GUILayout.RepeatButton("X Axis"))        
         {
             // (1, 0, 0)
             Selection.activeTransform.Rotate(Vector3.right, Random.Range(0f, 360f));
         }
         
-        if (GUILayout.RepeatButton("Randomise Rotation Y Axis"))
+        if (GUILayout.RepeatButton("Y Axis"))
         {
             // (0, 1, 0)
             Selection.activeTransform.Rotate(Vector3.up, Random.Range(0f, 360f));
         }
         
-        if (GUILayout.RepeatButton("Randomise Rotation Z Axis"))
+        if (GUILayout.RepeatButton("Z Axis"))
         {
             // (0, 0, 1)
             Selection.activeTransform.Rotate(Vector3.forward, Random.Range(0f, 360f));
         }
         
-        if (GUILayout.RepeatButton("Randomise Rotation All Axis"))
+        if (GUILayout.RepeatButton("All Axis"))
         {
             // (1, 1, 1)
             Selection.activeTransform.Rotate(Vector3.one, Random.Range(0f, 360f));
         }
+        GUILayout.EndHorizontal();
         
         GUILayout.Space(10);
-        GUILayout.Label("Scale Stuff", EditorStyles.boldLabel);
-        if (GUILayout.RepeatButton("Randomise X Scale by 1-10"))
+        GUILayout.Label($"Randomise Scale Stuff by > {scaleValue} <", EditorStyles.boldLabel);
+        scaleValue = GUILayout.HorizontalSlider(scaleValue, 1, 100);
+        GUILayout.Space(15f);
+        GUILayout.BeginHorizontal();
+        if (GUILayout.RepeatButton("X Scale"))
         {
-            Selection.activeTransform.localScale = new Vector3(Random.Range(0, 10), 0, 0);
+            Selection.activeTransform.localScale = new Vector3(Random.Range(0, scaleValue), 0, 0);
         }
         
-        if (GUILayout.RepeatButton("Randomise Y Scale 1-10"))
+        if (GUILayout.RepeatButton("Y Scale"))
         {
-            Selection.activeTransform.localScale = new Vector3(0, Random.Range(0, 10), 0);
+            Selection.activeTransform.localScale = new Vector3(0, Random.Range(0, scaleValue), 0);
         }
         
-        if (GUILayout.RepeatButton("Randomise Z Scale 1-10"))
+        if (GUILayout.RepeatButton("Z Scale"))
         {
-            Selection.activeTransform.localScale = new Vector3(0, 0, Random.Range(0, 10));
+            Selection.activeTransform.localScale = new Vector3(0, 0, Random.Range(0, scaleValue));
         }
         
-        if (GUILayout.RepeatButton("Randomise All Size/Scale 1-10"))
+        if (GUILayout.RepeatButton("All Size/Scale"))
         {
-            Selection.activeTransform.localScale = new Vector3(Random.Range(0, 10), Random.Range(0, 10), Random.Range(0, 10));
+            Selection.activeTransform.localScale = new Vector3(Random.Range(0, scaleValue), Random.Range(0, scaleValue), Random.Range(0, scaleValue));
         }
+        GUILayout.EndHorizontal();
         
         GUILayout.Space(10);
         GUILayout.Label("Colour Stuff", EditorStyles.boldLabel);
+        GUILayout.BeginHorizontal();
         if (GUILayout.RepeatButton("Randomise Colour"))
         {
             Selection.activeGameObject.GetComponent<MeshRenderer>().material.color = new Color(Random.Range(0, 10f), Random.Range(0, 10f), Random.Range(0, 10f));
         }
+        GUILayout.EndHorizontal();
+        
+        GUILayout.Space(10);
+
+        
     }
 }
